af366923f6e2eda94d977bcb5c2d0716443310dd
Alex Robbins <alexander.j.robbins@gmail.com>
2012-12-08 14:06:09 -0600

    Board construction and seeding.

--- src/cgol/core.clj
+++ src/cgol/core.clj
@@ -1,6 +1,35 @@
 (ns cgol.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn all-pos-int?
+  [& args]
+  (every?
+   #(and (integer? %) (pos? %))
+   args))
+
+(defn board
+  "Make a game board"
+  [w h]
+  {:pre [(all-pos-int? w h)]}
+  (vec (repeat w
+               (vec (repeat h nil)))))
+
+(defn get-coord
+  "Get the liveness value of the given coordinate"
+  [x y]
+  {:pre [(all-pos-int? x y)]}
+  (get-in x y))
+
+(defn set-coord
+  "Set the liveness value of the given coordinate"
+  [board x y liveness]
+  {:pre [(all-pos-int? x y)]}
+  (assoc-in board [x y] liveness))
+
+(defn seed
+  "A seq of x,y coords of live cells"
+  [board & lives]
+  (reduce
+   (fn [current-board [x y]]
+     (set-coord current-board x y :live))
+   board
+   lives))
--- test/cgol/core_test.clj
+++ test/cgol/core_test.clj
@@ -2,6 +2,11 @@
   (:use clojure.test
         cgol.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest board-test
+  (testing "Test board generation."
+    (are [w h out]
+         (= (board w h) out)
+          1  2  [[nil nil]]
+          2  1  [[nil] [nil]])
+    (is (thrown? AssertionError (board "string" 1)))
+    (is (thrown? AssertionError (board -1 -1)))))
