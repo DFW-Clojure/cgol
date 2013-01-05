7d724d0ad5be3d5989c74b82bb94344e9efd8fc0
Joshua Griffith <josh.griffith@gmail.com>
2013-01-05 16:07:32 -0600

    Add neighbor coordinate calculations

--- src/cgol/core.clj
+++ src/cgol/core.clj
@@ -7,16 +7,51 @@
   [n]
   (vec (repeat (* n n) :off)))
 
+(defn get-n [board]
+  (sqrt (count board)))
+
 (defn partition-board
   "Take a flat vector board and partition it into a seq of seqs"
   [board]
   {:pre [(let [[n r] (exact-integer-sqrt (count board))]
             (zero? r))]}
 
-  (let [n (sqrt (count board))]
+  (let [n (get-n board)]
     (partition n board)))
 
 (defn print-board
-  "Pretty print a board"
+  "Pretty print a flat board"
   [board]
-  )
+  (doseq [r (partition-board board)]
+    (println r)))
+
+(defn- coords->flat
+  [n x y]
+  (+ x (* n y)))
+
+(defn get-in-board
+  "Get the board value for a given board, x, and y coordinate"
+  [board x y]
+  (let [n (get-n board)
+        i (coords->flat n x y)]
+    (board i)))
+
+(defn set-in-board
+  [board x y value]
+  (let [n (get-n board)
+        i (coords->flat n x y)]
+  (assoc board i value)))
+
+(defn get-neighbors
+  [board x y]
+  (let [n          (get-n board)
+        neighbor-d (for [dx (range -1 2) dy (range -1 2) 
+                         :when (not (every? zero? [dx dy]))] 
+                     [dx dy])
+        all-coords (map (fn [[dx dy]] [(+ x dx) (+ y dy)])
+                             neighbor-d)
+        neighbors  (filter (fn [[x y]] (and (<= 0 x (dec n))
+                                            (<= 0 y (dec n))))
+                           all-coords)]
+    (map (fn [[x y]] (get-in-board board x y)) neighbors)))
+  
--- test/cgol/core_test.clj
+++ test/cgol/core_test.clj
@@ -2,6 +2,12 @@
   (:use clojure.test
         cgol.core))
 
+(def block-board
+  [:off :off :off :off
+   :off :on  :on  :off
+   :off :on  :on  :off
+   :off :off :off :off])
+
 (deftest make-board-test
   (testing "board creation"
     (is 
@@ -23,3 +29,28 @@
      (= (partition-board (make-board 2))
         [[:off :off] [:off :off]]))
     (is (thrown? AssertionError (partition-board [:off :off :off])))))
+
+(deftest print-board-test
+  (testing "pretty print a flat board as a partitioned board"
+    (is
+     (= (with-out-str (print-board (make-board 3)))
+        "(:off :off :off)\n(:off :off :off)\n(:off :off :off)\n"))))
+
+(deftest get-in-board-test
+  (testing "get the value at given x and y coordinate in a flat board"
+    (are [ x y out]
+     (= (get-in-board block-board x y)
+        out)
+     
+     0 0 :off
+     1 1 :on
+     2 1 :on
+     3 3 :off)))
+
+(deftest neighbors-test
+  (are [x y neighbors]
+       (= (frequencies (get-neighbors block-board x y))
+          neighbors)
+       
+       1 1 {:on 3 :off 5}
+       0 0 {:on 1 :off 2}))
